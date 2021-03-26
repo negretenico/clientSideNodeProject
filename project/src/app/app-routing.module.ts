51eaad841ec089c4ff7376680bce52c8719676da
Nico <negretenico@gmail.com>
2021-03-26 14:17:36 -0400

    added contact us

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact/contact-us/contact-us.component';
+import { ThankyouComponent } from './contact/thankyou/thankyou.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'contact',children:
+  [
+    {path: '',component: ContactUsComponent},
+    {path: 'thankYou',component: ThankyouComponent},
+
+   ]
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
