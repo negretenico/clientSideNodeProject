47ca59c02fbcbe91ed7153b0c5d0179e7293553e
espinozal18 <espinozal18@students.ecu.edu>
2021-03-29 08:49:38 -0700

    Added home component with image carousel
    Name: Leo Espinoza
    Email: 1938166@tcs.com
    Date: 3/29/2021
    
    home component now has image carousel. Needs fetching images from db.

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
