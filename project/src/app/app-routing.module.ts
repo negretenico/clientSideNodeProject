60194b3270353ae2a2772d5a6abe6ca736b8444d
Nico <negretenico@gmail.com>
2021-03-29 18:35:36 -0400

    changes some small things

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -26,10 +26,11 @@ const routes: Routes = [
 {
   path: 'chat', component: ChatComponent
 },
-{path:'**',component: PageNotFoundComponent},
 {
   path: 'sports', component: SportsComponent
-}
+},
+{path:'**',component: PageNotFoundComponent},
+
 
 
 ];
