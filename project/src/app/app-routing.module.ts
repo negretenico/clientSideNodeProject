8adb8aea951e105920a2357ba68f3871607e5db7
Nico <negretenico@gmail.com>
2021-03-29 12:00:49 -0400

    added stuff

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutUsComponent},
+  {path: 'contact', component:ContactUsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
