215b1796a138bd44a8427a809c83bb2d795ddfdc
espinozal18 <espinozal18@students.ecu.edu>
2021-03-30 10:20:02 -0700

    test

--- project/src/app/home/home.component.spec.ts
+++ project/src/app/home/home.component.spec.ts
@@ -6,12 +6,12 @@ describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [ HomeComponent ]
+  //   })
+  //   .compileComponents();
+  // });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
