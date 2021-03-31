1d43c4bab58114a38f423f60f82b7c69dab504d2
Hemanth Ravi Gowda <hemanthravigowda@Hemanths-Air.connectivityu.com>
2021-03-31 13:34:39 -0500

    Dynamic weather

--- project/src/app/weather/weather.component.ts
+++ project/src/app/weather/weather.component.ts
@@ -15,8 +15,29 @@ export class WeatherComponent implements OnInit {
   ngOnInit(): void {
     this.weatherService.getWeather().subscribe( (res : any) => {
       this.data = res;
-      console.log(res);
     });
   }
 
+  updateWeather(userInput) : void {
+    var city = "Chicago";
+    var state = "IL";
+    var country = "US";
+    var inputArr = userInput.replace(/\s/g, "").split(",", 3);
+
+    if (inputArr != undefined && inputArr.length != 0) {
+      if (inputArr[0] != '') {
+        city = inputArr[0];
+      }
+      if (inputArr.length > 1 && inputArr[1] != '') {
+        state = inputArr[1];
+      }
+      if (inputArr.length > 2 && inputArr[2] != '') {
+        country = inputArr[2];
+      }
+    }
+
+    this.weatherService.getWeather(city, state, country).subscribe( (res : any) => {
+      this.data = res;
+    });
+  }
 }
